cc562ea6bb53b8b1f1dcc2db285b5684bae010d0
Manya Rani <81632252+ranimanya@users.noreply.github.com>
2022-05-29 17:16:13 +0530

    Add files via upload

--- MyVision.py
+++ MyVision.py
@@ -61,9 +61,24 @@ def MyFun():
     TestCamera().run()
 
     # saving the captured image
-    img = cv2.imread("files/IMG.png")
+    img_path = 'files/IMG.png'
+    img = cv2.imread(img_path)
 
-    # Deepface facial attribute analysis module
+    # Specify the image to be recognized
+    img1_path = 'Dwayne Johnson.jpg'
+    img2_path = img_path
+    img1 = cv2.imread(img1_path)
+    img2 = cv2.imread(img2_path)
+
+    model_name = "Facenet"
+
+    # Deepface face verification
+    resp = DeepFace.verify(img1_path=img1_path, img2_path=img2_path, model_name=model_name)
+
+    # converting the result into a list
+    all_resp = list(resp.items())
+
+    # Deepface facial attribute analysis
     result = DeepFace.analyze(img, actions=('emotion', 'age', 'gender', 'race'))
 
     # converting the result into a list
@@ -75,46 +90,71 @@ def MyFun():
     gender = all_pairs[4][1]
     race = all_pairs[6][1]
 
+    var1 = all_resp[0][1]
+    print(var1)
+
     # Code for voice output
+    if var1==True:
 
-    # output for gender
-    tts_gender1 = gTTS(gender, lang='en')
-    tts_gender2 = gTTS('the person is', lang='en')
-    tts_gender1.save("files/hello4.mp3")
-    tts_gender2.save("files/hello5.mp3")
-    playsound("files/hello5.mp3")
-    playsound("files/hello4.mp3")
+        # If the person is known this code runs
+        # Verifying face
+        var = gTTS('the person is known', lang='en')
+        var.save("files/MyName.mp3")
+        playsound("files/MyName.mp3")
 
-    # output for age
-    AgeNum = str(age)
-    tts_age1 = gTTS(AgeNum, lang='en')
-    tts_age2 = gTTS('their age is', lang='en')
-    tts_age1.save("files/hello2.mp3")
-    tts_age2.save("files/hello3.mp3")
-    playsound("files/hello3.mp3")
-    playsound("files/hello2.mp3")
+        # output for emotion
+        tts_emotion1 = gTTS(emotion, lang='en')
+        tts_emotion2 = gTTS('dominant emotion is', lang='en')
+        tts_emotion2.save("files/hello1.mp3")
+        tts_emotion1.save("files/hello.mp3")
+        playsound("files/hello1.mp3")
+        playsound("files/hello.mp3")
+    else:
 
-    # output for emotion
-    tts_emotion1 = gTTS(emotion, lang='en')
-    tts_emotion2 = gTTS('dominant emotion is', lang='en')
-    tts_emotion2.save("files/hello1.mp3")
-    tts_emotion1.save("files/hello.mp3")
-    playsound("files/hello1.mp3")
-    playsound("files/hello.mp3")
+        # If the person is unknown, certain features are given
+        # as output to give some idea about the person
+        var = gTTS('the person is unknown', lang='en')
+        var.save("files/MyName1.mp3")
+        playsound("files/MyName1.mp3")
 
-    # output for race
-    tts_race1 = gTTS(race, lang='en')
-    tts_race2 = gTTS('dominant race is', lang='en')
-    tts_race1.save("files/hello6.mp3")
-    tts_race2.save("files/hello7.mp3")
-    playsound("files/hello7.mp3")
-    playsound("files/hello6.mp3")
+        # output for gender
+        tts_gender1 = gTTS(gender, lang='en')
+        tts_gender2 = gTTS('the person is', lang='en')
+        tts_gender1.save("files/hello4.mp3")
+        tts_gender2.save("files/hello5.mp3")
+        playsound("files/hello5.mp3")
+        playsound("files/hello4.mp3")
 
-    # printing values
-    print(all_pairs[1])
-    print(all_pairs[3])
-    print(all_pairs[4])
-    print(all_pairs[6])
+        # output for age
+        AgeNum = str(age)
+        tts_age1 = gTTS(AgeNum, lang='en')
+        tts_age2 = gTTS('their age is', lang='en')
+        tts_age1.save("files/hello2.mp3")
+        tts_age2.save("files/hello3.mp3")
+        playsound("files/hello3.mp3")
+        playsound("files/hello2.mp3")
+
+        # output for emotion
+        tts_emotion1 = gTTS(emotion, lang='en')
+        tts_emotion2 = gTTS('dominant emotion is', lang='en')
+        tts_emotion2.save("files/hello1.mp3")
+        tts_emotion1.save("files/hello.mp3")
+        playsound("files/hello1.mp3")
+        playsound("files/hello.mp3")
+
+        # output for race
+        tts_race1 = gTTS(race, lang='en')
+        tts_race2 = gTTS('dominant race is', lang='en')
+        tts_race1.save("files/hello6.mp3")
+        tts_race2.save("files/hello7.mp3")
+        playsound("files/hello7.mp3")
+        playsound("files/hello6.mp3")
+
+        # printing values
+        print(all_pairs[1])
+        print(all_pairs[3])
+        print(all_pairs[4])
+        print(all_pairs[6])
 
 
 class TestCamera(App):
